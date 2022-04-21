4815f2097e357c8ec69e5e2673ac1f3e905bb723
Giovanni <gigiosinicasi@gmail.com>
2022-04-21 17:47:09 -0300

    HTML/

---
+++ Projeto/teste.php
@@ -0,0 +1,86 @@
+<html lang="pt-br">
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-widt", initial-scale=1.0>
+	<link rel="stylesheet" href="main.css">
+	
+	<link reL="stylesheet" href="owl.carousel.min.css">
+	<link reL="stylesheet" href="owl.theme.default.min.css">
+	<link reL="stylesheet" href="responsive.css">
+	
+	<title>NETFLIX CLONE</title>
+
+</head>
+
+<body>	
+	<header>
+		<div class="container">
+			<h2 class="logo">NETFLIX</h2>		
+			<nav class="menu">
+				<a href="#">Iniciar</a>
+				<a href="#">Series</a>
+				<a href="#">Documentario</a>
+			</nav>
+		</div>
+	</header>
+	
+	<main>
+		<div class="filme-principal">
+			<div class="container">
+				<h3 class ="titulo">HOUSE OF CARDS</h3>
+				<p class="descricao">Nada pode impedir o pollitico sem escrupulos Frank Underwood de conquistar Washington</p>
+			</div>
+			
+			<div class="butoes">
+				<button role="button" class="botao">
+					<i class="fa-solid fa-play"></i>
+					ASSISTIR AGORA
+				</button>
+				<button role="button" class="botao">
+				<i class="fa-solid fa-info"></i>
+					MAIS INFORMAÇÕES
+				</button>
+			</div>
+			
+			
+		</div>
+	</main>
+	
+	<div class="carrossel-filme">
+		<div class="owl-carousel owl-theme">
+			<div class="item">
+				<img class="box-filme" src="1.jpg"></img>
+			</div>
+			<div class="item">
+				<img class="box-filme" src="2.jpg"></img>
+			</div>
+			<div class="item">
+				<img class="box-filme" src="3.jpg"></img>
+			</div>
+			<div class="item">
+				<img class="box-filme" src="4.jpg"></img>
+			</div>
+			<div class="item">
+				<img class="box-filme" src="5.jpg"></img>
+			</div>
+			<div class="item">
+				<img class="box-filme" src="3.png"></img>
+			</div>
+			<div class="item">
+				<img class="box-filme" src="5.png"></img>
+			</div>
+			<div class="item">
+				<img class="box-filme" src="6.png"></img>
+			</div>
+		</div>
+	</div>
+	
+<script src="https://kit.fontawesome.com/1f243b9cd7.js" crossorigin="anonymous"></script>
+<script src="jquery.min.js"></script>
+<script src="owl.carousel.min.js"></script>
+<script src="setup.js"></script>
+
+</body>
+</html>
+
